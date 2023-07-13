ad4b38ae580ff04f6674a64bd86fda442a7cfb95
Christian F <109520303+Barsch2006@users.noreply.github.com>
2023-07-13 05:54:19 +0000

    fix theme and strarted on server irgnoring Dolphin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -5,8 +7,31 @@ export default defineNuxtConfig({
     "nuxt-vuetify",
   ],
   vuetify: {
+    icons: {
+      defaultSet: "mdi",
+      aliases: aliases,
+      sets: {
+        mdi: mdi
+      }
+    }, 
     theme: {
-      defaultTheme: "dark",
+      defaultTheme: "dolphinTheme",
+      themes: {
+        dolphinTheme: {
+          dark: true,
+          colors: {
+            background: "#222222", // Background-Color (dunkler)
+            surface: "#525252", // Form-Background-Color (heller)
+            primary: "#207178", // Darker-Dolphin-Color 
+            secondary: "#50a4ab", // Lighter-Dolphin-Color
+            error: "#ef2d13", // Rot
+            info: "#2196f3", // Grün
+            success: "#19c719", // Helles Blau
+            warning: "#fb8200", // Orange
+          }
+        }
+
+      },
     }
   }
 })
