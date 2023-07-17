37b7f033877fada6975089fe35b5183481669565
heeecker <89743412+filip326@users.noreply.github.com>
2023-07-17 07:36:34 +0200

    /api/whoami

--- server/api/whoami.get.ts
+++ server/api/whoami.get.ts
@@ -1,5 +1,13 @@
 import Dolphin from "@/server/Dolphin/Dolphin";
 
 export default eventHandler(async (event) => {
-   
+    if (!event.context.auth.authenticated || event.context.auth.mfa_required || !event.context.auth.user) {
+        throw createError({ statusCode: 401, message: "Unauthorized" });
+    }
+
+    return {
+        username: event.context.auth.user.username,
+        fullName: event.context.auth.user.fullName,
+        type: event.context.auth.user.type,
+    }
 });
