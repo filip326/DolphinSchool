5d459b80fcd752dc6d81192bd8e63bfce4dc5ecf
Barsch2006 <cchr98756@gmail.com>
2023-07-17 07:36:47 +0200

    whoami

--- server/api/whoami.get.ts
+++ server/api/whoami.get.ts
@@ -1,5 +1,20 @@
 import Dolphin from "@/server/Dolphin/Dolphin";
+import User from "../Dolphin/User/User";
 
 export default eventHandler(async (event) => {
-   
+    const user: User | undefined = event.context.auth.user;
+
+    if (!user) {
+        throw createError({ statusCode: 401, message: "Unauthorized" });
+    }
+
+    if (!event.context.auth.authenticated) {
+        throw createError({ statusCode: 401, message: "Unauthorized" });
+    }
+
+    return {
+        username: user.username,
+        fullName: user.fullName,
+        type: user.type,
+    };
 });
