cb7785b128415287caf48247995c75be918c0866
Barsch2006 <cchr98756@gmail.com>
2023-07-19 21:58:57 +0200

    add docs (layout)

---
+++ pages/docs/index.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'docs'
+});
+</script>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>Docs</h1>
+    </div>
+</template>
+
+<style scoped></style>
