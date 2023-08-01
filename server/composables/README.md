26ff01584b44ae60c535d41490cfe523325a8d2c
Barsch2006 <cchr98756@gmail.com>
2023-08-01 11:43:31 +0200

    add composables for server dir

---
+++ server/composables/README.md
@@ -0,0 +1,5 @@
+# ~server/composables
+
+Use it like the client-side composables directory.
+
+**Warning** as a difference to the client-side: nuxt autoimport does not work on this dir.
---
+++ server/composables/checkAuth.ts
@@ -0,0 +1,48 @@
+import { H3Event } from "h3";
+import { Permissions } from "../Dolphin/Permissions/PermissionManager";
+
+interface IOptions {
+    authRequired: boolean;
+    throwErrOnAuthFail: boolean;
+    requirePerm: Permissions;
+}
+
+export default async (event: H3Event, options: IOptions): Promise<boolean> => {
+    if (event.context.auth.authenticated && event.context.auth.user) {
+        if (options.requirePerm) {
+            if (event.context.auth.user.hasPermission(options.requirePerm)) {
+                return true;
+            } else {
+                if (options.throwErrOnAuthFail) {
+                    throw throw403();
+                }
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        if (options.authRequired) {
+            if (options.throwErrOnAuthFail) {
+                throw throw401();
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+function throw401() {
+    return createError({
+        statusCode: 401,
+        statusMessage: "Unauthorized",
+    });
+}
+
+function throw403() {
+    return createError({
+        statusCode: 403,
+        statusMessage: "Forbidden",
+    });
+}
