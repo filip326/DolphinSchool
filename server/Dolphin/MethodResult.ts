604a92794a2c91cd51e86ced1669ec36ef192b3e
Barsch2006 <christian0511@gmx.de>
2023-08-10 19:59:11 +0200

    add DolphinError-enum (not active)

--- server/Dolphin/MethodResult.ts
+++ server/Dolphin/MethodResult.ts
@@ -1,2 +1,14 @@
+enum DolphinErrorTypes {
+    Failed = 0,
+    NotFound = 1,
+    InvalidParameter = 2,
+    AlreadyExists = 3,
+    DatabaseConnectionFailed = 4,
+    DatabaseQueryFailed = 5
+}
+
+type DolphinError = [DolphinErrorTypes, string?];
+
 type MethodResult<T> = [T, null] | [undefined, Error];
 export default MethodResult;
+export { DolphinError, DolphinErrorTypes };
