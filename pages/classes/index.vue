26d3809caff40ad02908b0112a85a0162e993466
Barsch2006 <christian0511@gmx.de>
2023-09-09 15:34:04 +0200

    rem teacher check

--- pages/classes/index.vue
+++ pages/classes/index.vue
@@ -1,15 +1,11 @@
 <script lang="ts">
 export default {
     async beforeCreate() {
-        const checkAuthResult = await checkAuth({
+        await checkAuth({
             redirectOnMfaRequired: true,
             redirectOnPwdChange: true,
             throwErrorOnNotAuthenticated: true,
         });
-
-        if (checkAuthResult.user.type !== "teacher") {
-            throw createError({ statusCode: 403, statusMessage: "Forbidden" });
-        }
     },
     data() {
         return {
--- pages/classes/subjects/create.vue
+++ pages/classes/subjects/create.vue
@@ -3,15 +3,11 @@
 <script>
 export default {
     async beforeCreate() {
-        const checkAuthResult = await checkAuth({
+        await checkAuth({
             redirectOnMfaRequired: true,
             redirectOnPwdChange: true,
             throwErrorOnNotAuthenticated: true,
         });
-
-        if (checkAuthResult.user.type !== "teacher") {
-            throw createError({ statusCode: 403, statusMessage: "Forbidden" });
-        }
     },
 };
 </script>
