87beeefeb9b4771d1f2ad49552505fd821a2489e
heeecker <89743412+filip326@users.noreply.github.com>
2023-10-01 20:39:50 +0200

    darkened ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -82,7 +82,7 @@ export default defineNuxtConfig({
                     dark: true,
                     colors: {
                         background: "#222222", // Background-Color (dunkler)
-                        surface: "#525252", // Form-Background-Color (heller)
+                        surface: "#0e0e0e", // Form-Background-Color (heller)
                         primary: "#207178", // Darker-Dolphin-Color
                         secondary: "#50a4ab", // Lighter-Dolphin-Color
                         error: "#ef2d13", // Rot
