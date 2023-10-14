7603f0bf9094bd457382089ce61b6bc38e504541
Barsch2006 <christian0511@gmx.de>
2023-10-14 15:34:42 +0200

    user overview

--- pages/admin/users/[user_id].vue
+++ pages/admin/users/[user_id].vue
@@ -50,43 +50,41 @@ export default {
                 this.error.message = "Fehler beim Löschen des Benutzers";
             }
         },
-        async saveUser() {},
     },
 };
 </script>
 
 <template>
     <VAlert v-if="error.show" title="Fehler" type="error" :text="error.message" />
-    <VForm @submit.prevent="saveUser">
-        <VCard>
-            <VCardTitle>Benutzer {{ user.username }}</VCardTitle>
-            <VCardText>
-                <VTextField label="Voller Name" v-model="user.fullName" />
-                <VTextField label="Benutzername" v-model="user.username" />
-                <VSelect
-                    label="Typ"
-                    :items="['student', 'teacher', 'parent']"
-                    v-model="user.type"
-                />
-                <VTextField label="Kürzel" v-model="user.kuezel" v-if="user.type === 'teacher'" />
-                <VCheckbox label="2FA aktiviert" v-model="user.mfaEnabled" readonly />
-            </VCardText>
-            <VCardText v-if="user.type === 'student'">
-                <VBtn
-                    link
-                    :href="'/admin/users/' + parent"
-                    :key="i"
-                    v-for="(parent, i) in user.parents"
-                    >Elternteil {{ i + 1 }}</VBtn
-                >
-                <!-- TODO -->
-                <!-- textfield with ASMSQ to add parents -->
-            </VCardText>
-            <VCardActions>
-                <VBtn variant="flat" type="submit" color="primary">Änderungen speichern</VBtn>
-                <VBtn link href="/admin/users">Änderungen verwerfen</VBtn>
-                <VBtn variant="flat" color="error" @click="deleteUser">Benutzer löschen</VBtn>
-            </VCardActions>
-        </VCard>
-    </VForm>
+    <VCard>
+        <VCardTitle>Benutzer {{ user.username }}</VCardTitle>
+        <VCardText>
+            <VTextField label="Voller Name" v-model="user.fullName" readonly />
+            <VTextField label="Benutzername" v-model="user.username" readonly />
+            <VSelect
+                label="Typ"
+                :items="['student', 'teacher', 'parent']"
+                v-model="user.type"
+                readonly
+            />
+            <VTextField
+                label="Kürzel"
+                v-model="user.kuezel"
+                v-if="user.type === 'teacher'"
+                readonly
+            />
+            <VCheckbox label="2FA aktiviert" v-model="user.mfaEnabled" readonly />
+        </VCardText>
+        <VCardText v-if="user.type === 'student'">
+            <VBtn link :href="'/admin/users/' + parent" :key="i" v-for="(parent, i) in user.parents"
+                >Elternteil {{ i + 1 }}</VBtn
+            >
+            <!-- TODO -->
+            <!-- textfield with ASMSQ to add parents -->
+        </VCardText>
+        <VCardActions>
+            <VBtn variant="flat" link href="/admin/users" color="primary">Zurück</VBtn>
+            <VBtn variant="flat" color="error" @click="deleteUser">Benutzer löschen</VBtn>
+        </VCardActions>
+    </VCard>
 </template>
