21eac367755ba14c72e52645d58a07db1c80892c
heeecker <89743412+filip326@users.noreply.github.com>
2023-10-30 08:50:02 +0000

    small fix

--- server/api/asmsq/suggest.get.ts
+++ server/api/asmsq/suggest.get.ts
@@ -1,7 +1,7 @@
 import ASMSQ from "~/server/Dolphin/ASMSQ/ASMSQ";
 
 export default eventHandler(async (event) => {
-    const checkAuthResult = await event.context.auth.checkAuth(event, {});
+    const checkAuthResult = await event.context.auth.checkAuth();
     if (!checkAuthResult.success || !checkAuthResult.user) {
         throw createError({ statusCode: 401, message: "Unauthorized" });
     }
@@ -19,3 +19,4 @@ export default eventHandler(async (event) => {
 
     return result;
 });
+
