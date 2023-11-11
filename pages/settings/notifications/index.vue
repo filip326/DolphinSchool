00eebfcb8cb88d51f19c99700790aa7197c83aed
heeecker <89743412+filip326@users.noreply.github.com>
2023-11-11 20:23:54 +0100

    Refactor code for improved readability and performance.

---
+++ pages/settings/notifications/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+
+
+</script>
+
+<template>
+
+    <VCard>
+        <VCardTitle>Push-Benachrichtigungen</VCardTitle>
+        <VCardText>
+
+        </VCardText>
+    </VCard>
+
+    <VCard>
+        <VCardTitle>Meine Geräte</VCardTitle>
+        <VCardText>
+            Hier sind alle Geräte aufgelistet, an welche wir Push-Benachrichtigungen senden.
+        </VCardText>
+    </VCard>
+
+</template>
