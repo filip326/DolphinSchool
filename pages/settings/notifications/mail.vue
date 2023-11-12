ffeec2cf210266379b7182129dd684fa5edf1906
Barsch2006 <christian0511@gmx.de>
2023-11-12 21:05:06 +0100

    Add email notification feature with push notification option

--- pages/settings/notifications/mail.vue
+++ pages/settings/notifications/mail.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 export default {
     async beforeCreate() {
         await checkAuth({
@@ -8,4 +7,63 @@ export default {
             throwErrorOnNotAuthenticated: true,
         });
     },
+    data() {
+        return {
+            hasPushNotificationActive: false,
+            error: {
+                show: false,
+                message: "",
+            },
+        };
+    },
+    async beforeMount() {
+        const otherDeviceResponse = await useFetch("/api/pushservice/device", {
+            method: "get",
+        });
+        if (otherDeviceResponse.status.value === "success") {
+            if (!otherDeviceResponse.data.value) {
+                this.error = {
+                    show: true,
+                    message:
+                        "Es ist ein Fehler aufgetreten. Bitte versuche es später erneut.",
+                };
+                return;
+            }
+            if (otherDeviceResponse.data.value.deviceFound) {
+                this.hasPushNotificationActive = true;
+            } else {
+                this.hasPushNotificationActive = false;
+                return;
+            }
+        }
+    },
 };
+</script>
+
+<template>
+    <VCard>
+        <VCardTitle>E-Mail Benachrichtigungen</VCardTitle>
+        <VCardText v-if="hasPushNotificationActive">
+            <VIcon>mdi-bell</VIcon>
+            Du hast die Push-Benachrichtigungen aktiviert. Deaktiviere sie, wenn du via
+            E-Mail benachrichtigt werden möchtest.
+            <VBtn link to="/settings/notifications/push" color="error">
+                Zum Deaktivieren
+            </VBtn>
+        </VCardText>
+        <VCardText v-else>
+            E-Mail Benachrichtigungen sind noch nicht verfügbar.
+        </VCardText>
+    </VCard>
+</template>
+
+<style scoped>
+.check-result {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    padding: 20px;
+    text-align: center;
+}
+</style>
