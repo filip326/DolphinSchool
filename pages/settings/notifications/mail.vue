47b8c5e3fe9d5dcc03c11357f79565f20c866e66
Barsch2006 <christian0511@gmx.de>
2023-11-12 20:43:23 +0100

    some changes

---
+++ pages/settings/notifications/mail.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+export default {
+    async beforeCreate() {
+        await checkAuth({
+            redirectOnMfaRequired: true,
+            redirectOnPwdChange: true,
+            throwErrorOnNotAuthenticated: true,
+        });
+    },
+};
