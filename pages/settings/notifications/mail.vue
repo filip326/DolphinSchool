e5ba7210b8e54bfc444fa394d99b85f1c7ee7277
Barsch2006 <christian0511@gmx.de>
2023-11-12 21:14:22 +0100

    Remove push notification functionality and update email notification text

--- pages/settings/notifications/mail.vue
+++ pages/settings/notifications/mail.vue
@@ -9,51 +9,20 @@ export default {
     },
     data() {
         return {
-            hasPushNotificationActive: false,
             error: {
                 show: false,
                 message: "",
             },
         };
     },
-    async beforeMount() {
-        const otherDeviceResponse = await useFetch("/api/pushservice/device", {
-            method: "get",
-        });
-        if (otherDeviceResponse.status.value === "success") {
-            if (!otherDeviceResponse.data.value) {
-                this.error = {
-                    show: true,
-                    message:
-                        "Es ist ein Fehler aufgetreten. Bitte versuche es später erneut.",
-                };
-                return;
-            }
-            if (otherDeviceResponse.data.value.deviceFound) {
-                this.hasPushNotificationActive = true;
-            } else {
-                this.hasPushNotificationActive = false;
-                return;
-            }
-        }
-    },
+    async beforeMount() {},
 };
 </script>
 
 <template>
     <VCard>
         <VCardTitle>E-Mail Benachrichtigungen</VCardTitle>
-        <VCardText v-if="hasPushNotificationActive">
-            <VIcon>mdi-bell</VIcon>
-            Du hast die Push-Benachrichtigungen aktiviert. Deaktiviere sie, wenn du via
-            E-Mail benachrichtigt werden möchtest.
-            <VBtn link to="/settings/notifications/push" color="error">
-                Zum Deaktivieren
-            </VBtn>
-        </VCardText>
-        <VCardText v-else>
-            E-Mail Benachrichtigungen sind noch nicht verfügbar.
-        </VCardText>
+        <VCardText> E-Mail Benachrichtigungen sind noch nicht verfügbar. </VCardText>
     </VCard>
 </template>
 
