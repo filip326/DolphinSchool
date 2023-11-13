9b058bcd2e2ec37bb86cf6969eac416c1597c11e
heeecker <89743412+filip326@users.noreply.github.com>
2023-11-13 19:18:03 +0100

    Add email status endpoint with email obfuscation.

--- server/api/email/status.get.ts
+++ server/api/email/status.get.ts
@@ -1,6 +1,6 @@
+import EmailNotification from "~/server/Dolphin/EMailNotification";
 
 export default defineEventHandler(async (event) => {
-
     const { success, statusCode, user } = await event.context.auth.checkAuth();
     if (!success) {
         throw createError({
@@ -15,6 +15,42 @@ export default defineEventHandler(async (event) => {
         });
     }
 
-    const [ email, err ] = await EMailNo
+    const [email, err] = await EmailNotification.getByUser(user._id);
+    if (err || !email) {
+        return {
+            subscribed: false,
+            verified: false,
+            email: null,
+        };
+    }
 
+    let emailToDisplay = "";
+    // first two letters + x stars + @ + first letter of domain + .tld
+
+    const firstPartOfEmail = email.email.split("@")[0];
+    const domain = email.email.split("@")[1].split(".")[0];
+    const tld = email.email.split("@")[1].split(".")[1];
+
+    emailToDisplay =
+        firstPartOfEmail.substring(0, 2) +
+        "*".repeat(firstPartOfEmail.length - 2) +
+        "@" +
+        domain.substring(0, 1) +
+        "*".repeat(domain.length - 1) +
+        "." +
+        tld;
+
+    if (email.verified) {
+        return {
+            subscribed: true,
+            verified: true,
+            email: emailToDisplay,
+        };
+    } else {
+        return {
+            subscribed: true,
+            verified: false,
+            email: emailToDisplay,
+        };
+    }
 });
