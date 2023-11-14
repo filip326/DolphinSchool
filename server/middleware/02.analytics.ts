2d17a9b71a691f7ac032be96c0e643ad1db8cafc
heeecker <89743412+filip326@users.noreply.github.com>
2023-11-14 08:52:01 +0100

    Remove unused parameter in analytics middleware

--- server/middleware/02.analytics.ts
+++ server/middleware/02.analytics.ts
@@ -1,3 +1,3 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
     // todo: implement adding data from the event to the daily analytics-database
 });
