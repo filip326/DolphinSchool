79e8f93c01f65c5e0157188b08a207bf13d73375
heeecker <89743412+filip326@users.noreply.github.com>
2024-01-20 13:05:26 +0100

    Refactor password change form

--- pages/settings/security/chpwd/index.vue
+++ pages/settings/security/chpwd/index.vue
@@ -1,17 +1,18 @@
-<script>
+<script lang="ts">
 export default {
     data() {
         return {
+            formState: "old_password" as
+                | "old_password"
+                | "checking_old_password"
+                | "info"
+                | "new_password"
+                | "confirm_password",
             old_pwd: "",
             new_pwd: "",
             rules: {
-                required: (v) => !!v || "Dieses Feld ist erforderlich!",
-                wiederholen: (v) =>
-                    v === this.new_pwd || "Die Passwörter stimmen nicht überein!",
-                notsame: (v) =>
-                    v !== this.old_pwd ||
-                    "Das neue Passwort muss sich vom alten unterscheiden!",
-                password: (v) => {
+                required: (v: string) => !!v || "Dieses Feld ist erforderlich!",
+                password: (v: string) => {
                     if (v.length < 10) {
                         return "Das Passwort muss mindestens 10 Zeichen lang sein!";
                     }
@@ -33,6 +34,11 @@ export default {
         };
     },
     methods: {
+        async checkOldPassword() {
+            this.formState = "checking_old_password";
+            
+
+        },
         async changePassword() {
             const response = await useFetch("/api/setup/pwd/change", {
                 method: "POST",
@@ -43,14 +49,14 @@ export default {
             });
 
             if (!response.data.value || response.error.value) {
-                if (response.error.value.statusCode === 200) {
+                if (response.error?.value?.statusCode === 200) {
                     await navigateTo("/home");
                     return;
                 }
 
                 this.error.shown = true;
                 this.error.message =
-                    response.error.value.statusCode === 400
+                    response.error?.value?.statusCode === 400
                         ? response.error.value.message
                         : "Passwort ändern fehlgeschlagen!";
                 return;
@@ -76,76 +82,29 @@ export default {
 </script>
 
 <template>
-    <div class="settings_form">
-        <section class="settings_info">
-            <div>
-                <h1>Passwort ändern</h1>
-                <div>
-                    <p>
-                        Beachten Sie die folgenden Regeln für Ihres Passwort. Sie können
-                        Ihr Passwort jederzeit in den Einstellungen ändern.
-                    </p>
-                </div>
-                <ul>
-                    <li>Das Passwort muss mindestens 10 Zeichen lang sein.</li>
-                    <li>
-                        Das Passwort muss mindestens einen Kleinbuchstaben (a-z)
-                        enthalten.
-                    </li>
-                    <li>
-                        Das Passwort muss mindestens einen Großbuchstaben (A-Z) enthalten.
-                    </li>
-                    <li>Das Passwort muss mindestens eine Ziffer (0-9) enthalten.</li>
-                </ul>
-                <div>
-                    <p>
-                        Es wird empfohlen neben einem sicherem Passwort die
-                        Zwei-Faktor-Authentifizierung in den Einstellungen zu aktivieren.
-                        Weitere Informationen finden Sie beispielsweise auf der
-                        <a
-                            href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/Accountschutz/Sichere-Passwoerter-erstellen/sichere-passwoerter-erstellen_node.html"
-                        >
-                            Website des BSI
-                        </a>
-                    </p>
-                </div>
-            </div>
-        </section>
-
-        <VForm @submit.prevent="changePassword()">
-            <VAlert
-                v-if="error.shown"
-                type="error"
-                variant="text"
-                :text="error.message"
-            />
-
-            <p>
-                Geben Sie Ihr bisheriges Passwort ein. Wählen Sie anschließend ein neues
-                Passwort und wiederholen Sie dieses.
-            </p>
-
-            <VTextField
-                v-model="old_pwd"
-                label="bisheriges Passwort"
-                type="password"
-                :rules="[rules.required]"
-            />
-            <VTextField
-                v-model="new_pwd"
-                label="neues Passwort"
-                type="password"
-                :rules="[rules.required, rules.notsame, rules.password]"
-            />
-            <VTextField
-                label="Passwort wiederholen"
-                type="password"
-                :rules="[rules.required, rules.wiederholen, rules.password]"
-            />
-
-            <VBtn type="submit" color="primary" class="mr-4">Passwort ändern</VBtn>
-        </VForm>
-    </div>
+    <VCard>
+        <VCardTitle> Passwort ändern </VCardTitle>
+        <VWindow v-model="formState">
+            <VWindowItem value="old_password">
+                <VCardText>
+                    <p>Geben Sie bitte zunächst Ihr altes Passwort ein.</p>
+                    <VTextField
+                        type="password"
+                        label="Altes Passwort"
+                        v-model="old_pwd"
+                        :rules="[rules.required]"
+                    />
+                    <VBtn
+                        color="primary"
+                        @click="checkOldPassword()"
+                        :disabled="!old_pwd"
+                    >
+                        Weiter
+                    </VBtn>
+                </VCardText>
+            </VWindowItem>
+        </VWindow>
+    </VCard>
 </template>
 
 <style scoped>
